d826ff656cc31c7b5e515278fdac4dc589f2bc0c
memmoxt <willianfujii@gmail.com>
2024-06-26 22:24:31 -0300

    svelte cssbattle

--- src/routes/CSS_Battle.svelte
+++ src/routes/CSS_Battle.svelte
@@ -2,57 +2,60 @@
 	<div></div>
 	<div></div>
 	<div></div>
+	<div></div>
+    <div></div>
 </div>
 
 <style>
 	* {
 		margin: 0;
 		padding: 0;
-		background: #1a4341;
-        box-sizing:border-box;
+		box-sizing: border-box;
+		--orange: #ea7457;
+		--red: #b53733;
 	}
 
 	.main {
 		height: 300px;
 		width: 400px;
-		display: flex;
-		flex-wrap: wrap;
-		justify-content: center;
+		display: grid;
+        grid-template-columns:1fr 200px 1fr;
+        grid-template-rows:repeat(3,1fr);
+        background:var(--orange);
+
+		& > div {
+			height: 100px;
+			width: 100px;
+			background-color: var(--red);
+		}
 		& > div:nth-child(1) {
-			height: 150px;
-			width: 150px;
-			border-bottom: 30px solid #998235;
-			border-right: 30px solid #998235;
-			border-left: 30px solid #998235;
-			background-color: transparent;
-			align-self: start;
+            grid-column:2/3;
+            justify-self: center;
 		}
 		& > div:nth-child(2) {
-			position: absolute;
-			align-self: end;
-			height: 30px;
-			width: 270px;
-			background-color: #998235;
-			margin: 30px 0;
+            grid-column:1;
+            grid-row:2;
 		}
-
 		& > div:nth-child(3) {
-			position: absolute;
-			align-self: start;
-			height: 210px;
-			width: 270px;
-			border-bottom: 30px solid #f3ac3c;
-			border-right: 30px solid #f3ac3c;
-			border-left: 30px solid #f3ac3c;
-			background-color: transparent;
-			&:before {
-				content: '';
-				position: absolute;
-				width: 30px;
-				height: 300px;
-				background-color: #f3ac3c;
-				transform: translate(90px, 0px);
-			}
+            grid-column:3;
+            grid-row:2;
 		}
+		& > div:nth-child(4) {
+            grid-column:2;
+            grid-row:3;
+            justify-self:center;
+		}
+        &> div:nth-child(5) , &>div:nth-child(5):before{
+            position:absolute;
+            height:300px;
+            width:20px;
+            background-color:var(--red);
+            justify-self: center;
+        }
+        &> div:nth-child(5):before{
+            content:'';
+            position:absolute;
+            transform:rotate(90deg);
+        }
 	}
 </style>
---
+++ src/routes/Daily_Targets/2024/06-june/day-27.svelte
@@ -0,0 +1,62 @@
+<div class="main">
+	<div></div>
+	<div></div>
+	<div></div>
+	<div></div>
+	<div></div>
+</div>
+
+<style>
+	* {
+		margin: 0;
+		padding: 0;
+		box-sizing: border-box;
+		--orange: #ea7457;
+		--red: #b53733;
+	}
+
+	.main {
+		height: 300px;
+		width: 400px;
+		display: grid;
+		grid-template-columns: 1fr 200px 1fr;
+		grid-template-rows: repeat(3, 1fr);
+		background: var(--orange);
+
+		& > div {
+			height: 100px;
+			width: 100px;
+			background-color: var(--red);
+		}
+		& > div:nth-child(1) {
+			grid-column: 2/3;
+			justify-self: center;
+		}
+		& > div:nth-child(2) {
+			grid-column: 1;
+			grid-row: 2;
+		}
+		& > div:nth-child(3) {
+			grid-column: 3;
+			grid-row: 2;
+		}
+		& > div:nth-child(4) {
+			grid-column: 2;
+			grid-row: 3;
+			justify-self: center;
+		}
+		& > div:nth-child(5),
+		& > div:nth-child(5):before {
+			position: absolute;
+			height: 300px;
+			width: 20px;
+			background-color: var(--red);
+			justify-self: center;
+		}
+		& > div:nth-child(5):before {
+			content: '';
+			position: absolute;
+			transform: rotate(90deg);
+		}
+	}
+</style>
