9fb460d2ec626c0bcc242d719b2f7b442b4fa1b7
memmoxt <willianfujii@gmail.com>
2024-06-30 20:55:57 -0300

    CSS update

---
+++ Daily_Targets/2024/06-june/day-30.svelte
@@ -0,0 +1,54 @@
+<div class="main">
+	<div></div>
+	<div></div>
+	<div></div>
+</div>
+
+<style>
+	* {
+		margin: 0;
+		padding: 0;
+		box-sizing: content-box;
+		--blue: #328fc1;
+		--darkblue: #0a6190;
+		background: var(--darkblue);
+	}
+
+	.main {
+		height: 100vh;
+		width: 100vw;
+		display: flex;
+		justify-content: center;
+		align-items: center;
+		& > div:nth-child(1) {
+			height: 119px;
+			aspect-ratio: 1;
+			border-radius: 50%;
+			background-color: var(--blue);
+			&:before {
+				content: '';
+				position: absolute;
+				height: 160px;
+				aspect-ratio: 1;
+				border-radius: 50%;
+				transform: translate(-20px, -170px);
+				background-color: var(--blue);
+			}
+		}
+		& > div:nth-child(2) {
+			position: absolute;
+			height: 60px;
+			aspect-ratio: 1;
+			border-radius: 50%;
+			background-color: var(--darkblue);
+		}
+		& > div:nth-child(3) {
+			position: absolute;
+			height: 160px;
+			aspect-ratio: 1;
+			border-radius: 50%;
+			background-color: var(--blue);
+			transform: translate(0, 150px);
+		}
+	}
+</style>
--- src/routes/CSS_Battle.svelte
+++ src/routes/CSS_Battle.svelte
@@ -1,9 +1,5 @@
 <div class="main">
-	<div>
-		<div></div>
-		<div></div>
-	</div>
-	<div><div></div></div>
+	<div class="center"></div>
 </div>
 
 <style>
@@ -11,13 +7,8 @@
 		margin: 0;
 		padding: 0;
 		box-sizing: border-box;
-		--blue: #5dbcf9;
-		--black: #191919;
-		--amber: #f2ad43;
-		--yellow: #fff58f;
-		--orange: #e08027;
-		--brown: #824b20;
-		background: var(--black);
+		--dark:#191919;
+        --yellow:#F2AD43;
 	}
 
 	.main {
@@ -26,48 +17,13 @@
 		display: flex;
 		justify-content: center;
 		align-items: center;
-		gap: 25px;
-		& > div:nth-child(1) {
-			height: 200px;
-			width: 125px;
-			display: flex;
-			align-items: center;
-			overflow: hidden;
-			& > div:nth-child(1) {
-				height: 150px;
-				transform-origin: top left;
-				transform: translate(25px, 75px) rotate(-45deg);
-				aspect-ratio: 1;
-				background-color: var(--blue);
-			}
-			& > div:nth-child(2) {
-				position: absolute;
-				height: 50px;
-				width: 100px;
-				border-radius: 10px;
-				background-color: var(--blue);
-			}
-		}
-		& > div:nth-child(2) {
-			height: 200px;
-			width: 100px;
-			overflow: hidden;
-			display: flex;
-			align-items: center;
-			& > div:nth-child(1) {
-				height: 80px;
-				aspect-ratio: 1;
-				border-radius: 50%;
-				transform: translate(-50%, 0);
-				background-color: var(--black);
-				box-shadow:
-					0 0 0 10px var(--blue),
-					0 0 0 25px var(--black),
-					0 0 0 35px var(--blue),
-					0 0 0 50px var(--black),
-					0 0 0 60px var(--blue);
-			}
+		position: absolute;
+		& > .center {
+			height: 100px;
+			width: 250px;
+			background-color: var(--yellow);
+            opacity:0.5;
+            box-shadow: 0 0 0;
 		}
 	}
 </style>
-
