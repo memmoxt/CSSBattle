1824f04e0bf717980a80c4598199236260c3bfb2
memmoxt <willianfujii@gmail.com>
2024-07-10 22:17:13 -0300

    day 11

---
+++ Daily_Targets/2024/07-july/day-11.svelte
@@ -0,0 +1,53 @@
+<div class="main">
+	<div></div>
+	<div></div>
+	<div></div>
+	<div></div>
+	<div></div>
+</div>
+
+<style>
+	* {
+		margin: 0;
+		padding: 0;
+		box-sizing: content-box;
+		--dark: #024817;
+		--green: #176c36;
+		--light: #48bf7d;
+	}
+	.main {
+		height: 300px;
+		width: 400px;
+		display: flex;
+		justify-content: center;
+		align-items: center;
+		background-color: var(--green);
+		& > div {
+			position: absolute;
+			top: 210px;
+			border-top: 50px solid var(--light);
+			border-right: 40px solid transparent;
+			border-bottom: 50px solid transparent;
+			border-left: 40px solid transparent;
+		}
+		& > div:nth-child(2) {
+			transform: translate(-110px, -110px) rotate(90deg);
+		}
+
+		& > div:nth-child(3) {
+			transform: translate(110px, -110px) rotate(-90deg);
+		}
+
+		& > div:nth-child(4) {
+			transform: translate(0px, -220px) rotate(180deg);
+		}
+		& > div:nth-last-child(1) {
+			height: 80px;
+			aspect-ratio: 1;
+			border-radius: 50%;
+			background-color: var(--dark);
+			transform: translate(0px, -100px);
+			border: none;
+		}
+	}
+</style>
--- src/routes/CSS_Battle.svelte
+++ src/routes/CSS_Battle.svelte
@@ -4,16 +4,6 @@
 	<div></div>
 	<div></div>
 	<div></div>
-	<div></div>
-
-	<div></div>
-
-	<div></div>
-	<div></div>
-	<div></div>
-	<div></div>
-	<div></div>
-	<div></div>
 </div>
 
 <style>
@@ -21,30 +11,44 @@
 		margin: 0;
 		padding: 0;
 		box-sizing: content-box;
-		--white: #ffffcd;
-		--yellow: #edaf38;
+		--dark: #024817;
+		--green: #176c36;
+		--light: #48bf7d;
 	}
 	.main {
 		height: 300px;
 		width: 400px;
-		display: grid;
-		gap: 140px 20px;
-        background-color:var(--yellow);
-		grid-template-columns: repeat(6, 50px);
-		grid-template-rows: 80px 80px;
+		display: flex;
+		justify-content: center;
 		align-items: center;
-
+        background-color:var(--green);
 		& > div {
-			height: 80px;
-			width: 50px;
-			background-color: var(--white);
-		}
-
-		& > div:nth-child(7) {
 			position: absolute;
-			background-color: var(--white);
-			height: 100px;
-			width: 400px;
+			top: 210px;
+			border-top: 50px solid var(--light);
+			border-right: 40px solid transparent;
+			border-bottom: 50px solid transparent;
+			border-left: 40px solid transparent;
 		}
+        & > div:nth-child(2){
+            transform:translate(-110px,-110px) rotate(90deg);
+        }
+
+        & > div:nth-child(3){
+            transform:translate(110px,-110px) rotate(-90deg);
+        }
+
+        & > div:nth-child(4){
+            transform:translate(0px,-220px) rotate(180deg);
+        }
+        &>div:nth-last-child(1){
+            height:80px;
+            aspect-ratio:1;
+            border-radius:50%;
+            background-color:var(--dark);
+            transform:translate(0px,-100px);
+            border:none;
+        }
+
 	}
 </style>
