caac51a67942fc4cb0b3e9003df1d404bfcb2ef0
memmoxt <willianfujii@gmail.com>
2024-07-29 21:54:26 -0300

    daily target 30

---
+++ Daily_Targets/2024/07-july/day-30.svelte
@@ -0,0 +1,40 @@
+<div class="main">
+	<div></div>
+	<div></div>
+</div>
+
+<style>
+	* {
+		margin: 0;
+		padding: 0;
+		box-sizing: content-box;
+		--blue: #4f77ff;
+		--white: #eff8fe;
+		background-color: var(--blue);
+	}
+	.main {
+		height: 300px;
+		width: 400px;
+		display: flex;
+		justify-content: center;
+		align-items: center;
+		& > div:nth-child(1) {
+			height: 200px;
+			width: 200px;
+			border-radius: 50%;
+			background-color: red;
+			background-color: var(--white);
+			clip-path: polygon(0 100px, 0px 200px, 200px 200px, 200px 100px);
+			z-index: 2;
+		}
+		& > div:nth-child(2) {
+			height: 200px;
+			width: 40px;
+			background-color: var(--white);
+			position: absolute;
+			z-index: 1;
+			clip-path: polygon(0 150px, 0px -200px, 200px -200px, 200px 100px);
+			transform: rotate(45deg);
+		}
+	}
+</style>
--- src/routes/CSS_Battle.svelte
+++ src/routes/CSS_Battle.svelte
@@ -1,5 +1,6 @@
 <div class="main">
 	<div></div>
+    <div></div>
 </div>
 
 <style>
@@ -7,10 +8,9 @@
 		margin: 0;
 		padding: 0;
 		box-sizing: content-box;
-		--orange: #edaf38;
-		--green: #7f7f24;
-		--yellow: #ffffcd;
-		background: var(--orange);
+        --blue:#4f77ff;
+        --white:#eff8fe;
+        background-color:var(--blue);
 	}
 	.main {
 		height: 300px;
@@ -18,24 +18,23 @@
 		display: flex;
 		justify-content: center;
 		align-items: center;
-		& > div {
-			height: 120px;
-			width: 280px;
-			background-color: var(--yellow);
-			&:before,
-			&:after {
-				position: absolute;
-				content: '';
-				width: 280px;
-				height: 40px;
-				border-radius: 50%;
-				background-color: var(--green);
-				transform: translateY(-20px);
-			}
-			&:after {
-				background-color: var(--yellow);
-				transform: translateY(100px);
-			}
-		}
+        &>div:nth-child(1){
+            height:200px;
+            width:200px;
+            border-radius:50%;
+            background-color:red;
+            background-color:var(--white);
+            clip-path: polygon(0 100px, 0px 200px , 200px 200px, 200px 100px);
+            z-index:2;
+        }
+        &>div:nth-child(2){
+            height:200px;
+            width:40px;
+            background-color:var(--white);
+            position:absolute;
+            z-index:1;
+            clip-path: polygon(0 150px, 0px -200px , 200px -200px, 200px 100px);
+            transform:rotate(45deg);
+        }
 	}
 </style>
