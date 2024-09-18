329f397152b4629f338f0c2f7479aed9b2ff9302
memmoxt <willianfujii@gmail.com>
2024-09-18 20:03:01 -0300

    daily target september day 18

---
+++ Daily_Targets/2024/09-september/day-17.svelte
@@ -0,0 +1,45 @@
+<div class="main">
+	<div></div>
+</div>
+
+<style>
+	* {
+		margin: 0;
+		padding: 0;
+		box-sizing: border-box;
+		--white: #fcebeb;
+		--orange: #ea9a52;
+		--blue: #515697;
+        background-color:var(--orange);
+	}
+	.main {
+		height: 300px;
+		width: 400px;
+		display: flex;
+		justify-content: center;
+		align-items: center;
+		& > div {
+			height: 100px;
+			width: 400px;
+			background-color: var(--blue);
+			&:before {
+				content: '';
+				position: absolute;
+                height:100px;
+                aspect-ratio:1;
+                background-color:var(--white);
+                border-radius:50%;
+                transform: translate(150%,-100%);
+			}
+            &:after{
+                content:'';
+                position:absolute;
+                height:100px;
+                aspect-ratio:1;
+                transform: translate(150%,100%);
+                background-color:var(--blue);
+            }
+		}
+	}
+</style>
+
--- Daily_Targets/2024/09-september/day-18.svelte
+++ Daily_Targets/2024/09-september/day-18.svelte
@@ -24,17 +24,18 @@
 			clip-path: polygon(
 				80px 0,
 				128px 82.5px,
-				158px 82.5px,
-				143px 110px,
-				158px 136px,
+				159px 82.5px,
+				143.5px 110px,
+				159px 136px,
 				128px 136px,
-				80px 220px,
-                31.5px 136px,
-                0px 136px,
-                16px 110px,
-                0px 82.5px,
+				80px 219.0px,
+                32px 136.0px,
+                1px 136.0px,
+                17px 110.0px,
+                1px 82.5px,
                 32px 82.5px
 			);
 		}
 	}
 </style>
+
