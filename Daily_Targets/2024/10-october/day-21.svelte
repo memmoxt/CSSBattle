9be21183ce71713af810b1b13065def636c4f6ca
memmoxt <willianfujii@gmail.com>
2024-10-24 03:05:00 -0300

    daily target october day 23

---
+++ Daily_Targets/2024/10-october/day-21.svelte
@@ -0,0 +1,39 @@
+<div class="main">
+	<div></div>
+</div>
+
+<style>
+	* {
+		margin: 0;
+		padding: 0;
+		box-sizing: border-box;
+		--white: #f0f0f0;
+		--red: #e05947;
+        background-color:var(--white);
+	}
+	.main {
+		height: 300px;
+		width: 400px;
+		display: flex;
+		justify-content: center;
+		align-items: center;
+		& > div {
+			height: 80px;
+			width: 200px;
+			background-color: var(--red);
+			border-radius: 120px/50px;
+            &:before{
+                content:'';
+                position:absolute;
+                height:170px;
+                width:40px;
+                top:65px;
+                left:180px;
+                border-radius:50px/50px;
+                background-color:var(--white);
+                box-shadow:0 0 0px 15px var(--red);
+            }
+		}
+	}
+</style>
+
---
+++ Daily_Targets/2024/10-october/day-22.svelte
@@ -0,0 +1,38 @@
+<div class="main">
+	<div></div>
+</div>
+
+<style>
+	* {
+		margin: 0;
+		padding: 0;
+		box-sizing: border-box;
+		--purple: #434b92;
+		--yellow: #f3ac3c;
+        background-color:var(--purple);
+	}
+	.main {
+		height: 300px;
+		width: 400px;
+		display: flex;
+		justify-content: center;
+		align-items: center;
+		& > div {
+			height: 300px;
+			width: 190px;
+			background-color: var(--yellow);
+			&:before {
+				content: '';
+				position: absolute;
+				height: 50px;
+				width: 230px;
+				transform: translate(-20px, 25px);
+				background-color: var(--yellow);
+				box-shadow:
+					0 100px 0 0 var(--yellow),
+					0 200px 0 0 var(--yellow);
+			}
+		}
+	}
+</style>
+
