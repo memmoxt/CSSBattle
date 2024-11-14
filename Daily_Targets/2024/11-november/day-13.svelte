f537210cb116fc13a17206042adb4d8e25db59a7
memmoxt <willianfujii@gmail.com>
2024-11-14 08:20:06 -0300

    november day14

---
+++ Daily_Targets/2024/11-november/day-13.svelte
@@ -0,0 +1,45 @@
+<div class="main">
+	<div></div>
+</div>
+
+<style>
+	* {
+		margin: 0;
+		padding: 0;
+		box-sizing: border-box;
+		--white: #f3ead2;
+		--red: #d97d71;
+		--purple: #857495;
+        background-color:var(--white);
+	}
+	.main {
+		height: 300px;
+		width: 400px;
+		display: flex;
+		justify-content: center;
+		align-items: center;
+		& > div {
+			position: absolute;
+			bottom: 65px;
+			height: 170px;
+			width: 360px;
+			background-color: var(--red);
+			&:before {
+				position: absolute;
+				content: '';
+				height: 40px;
+				width: 360px;
+				background-color: var(--purple);
+			}
+			&:after {
+				position: absolute;
+				content: '';
+				height: 140px;
+				width: 30px;
+				background-color: var(--purple);
+				box-shadow: 330px 0 0 0 var(--purple);
+			}
+		}
+	}
+</style>
+
