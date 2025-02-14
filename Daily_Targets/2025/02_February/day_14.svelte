5b65595b7a3755e009c45bcb32385f7ef0dc908e
memmoxt <willianfujii@gmail.com>
2025-02-14 04:55:21 -0300

    cssbattle day_14_2 february

---
+++ Daily_Targets/2025/02_February/day_14.svelte
@@ -0,0 +1,44 @@
+<div class="main">
+	<div></div>
+	<div></div>
+</div>
+
+<style>
+	* {
+		margin: 0;
+		padding: 0;
+		box-sizing: content-box;
+		--red: #963032;
+		--green: #75cf34;
+        background-color:var(--green);
+	}
+	.main {
+		height: 300px;
+		width: 400px;
+		display: flex;
+		justify-content: center;
+		align-items: center;
+		position: absolute;
+		overflow: hidden;
+
+        &>div{
+            height:40px;
+            width:120px;
+            background-color:var(--red);
+            transform:translate(60px,-30px);
+            &:before{
+                content:'';
+                position:absolute;
+                height:25px;
+                width:40px;
+                transform:translate(10px, -20px);
+                background-color:var(--red);
+                box-shadow:60px 0 var(--red);
+            }
+        }
+        &>div:nth-child(2){
+            transform:translate(-60px, 30px) scale(-1) ;
+        }
+	}
+</style>
+
