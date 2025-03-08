5d2be7e461288ad094e38224702646625eb280af
memmoxt <willianfujii@gmail.com>
2025-03-08 01:02:59 -0300

    cssbattle day_8 March

---
+++ Daily_Targets/2025/03_March/day_6.svelte
@@ -0,0 +1,51 @@
+<div class="main">
+	<div></div>
+</div>
+
+<style>
+	* {
+		margin: 0;
+		padding: 0;
+		box-sizing: content-box;
+		--orange: #e8ad6d;
+		--red: #de6b67;
+        background-color:var(--orange);
+	}
+	.main {
+		height: 300px;
+		width: 400px;
+		display: flex;
+		justify-content: center;
+		align-items: center;
+		flex-wrap: wrap;
+		position: absolute;
+		overflow: hidden;
+		& > div {
+			position: absolute;
+			height: 300px;
+			width: 40px;
+			transform: translate(-96px);
+			background-color: var(--red);
+			box-shadow:
+				60px 0 var(--red),
+				100px 0 var(--red),
+				130px 0 var(--red),
+				191px 0 var(--red);
+			/* opacity: 0.5; */
+            &:before,&:after{
+                content:'';
+                position:absolute;
+                background-color:var(--orange);
+                width:42px;
+                height:30px;
+                transform:translate(94px, 79px);
+                clip-path: polygon(50% 0px, 100% 50%, 50% 100%, 0 50%);
+            }
+            &:after{
+
+                transform:translate(94px, 190px);
+            }
+		}
+	}
+</style>
+
---
+++ Daily_Targets/2025/03_March/day_7.svelte
@@ -0,0 +1,52 @@
+<div class="main">
+	<div></div>
+	<div></div>
+</div>
+
+<style>
+	* {
+		margin: 0;
+		padding: 0;
+		box-sizing: content-box;
+		--white: #ffeff7;
+		--blue: #226aa9;
+		--yellow: #deb467;
+		--black: #000000;
+        background-color:var(--white);
+	}
+	.main {
+		height: 300px;
+		width: 400px;
+		display: flex;
+		justify-content: center;
+		align-items: center;
+		flex-wrap: wrap;
+		position: absolute;
+		overflow: hidden;
+		& > div:nth-child(1) {
+			position: absolute;
+			height: 160px;
+			width: 170px;
+			background-color: var(--blue);
+			transform: translate(-140px);
+		}
+		& > div:nth-child(2) {
+			position: absolute;
+			height: 160px;
+			width: 110px;
+			background-color: var(--yellow);
+			transform: translate(0px);
+            clip-path: polygon(0 0, 100% 50%, 0 100%);
+			&:before {
+				position: absolute;
+				content: '';
+				background-color: var(--black);
+				height: 100px;
+				transform: translate(60px, 30px);
+				aspect-ratio: 1;
+				border-radius: 50%;
+			}
+		}
+	}
+</style>
+
