be43edcf81bb162d2634ec5bf8fc6f3cb5bd11e5
memmoxt <willianfujii@gmail.com>
2025-04-08 21:19:14 -0300

    april-day-9 & 83+84 cssbattles

---
+++ Battles/Battle_015/83-Supernova.svelte
@@ -0,0 +1,56 @@
+<div class="main">
+	<div></div>
+</div>
+
+<style>
+    * {
+        margin: 0;
+        padding: 0;
+        box-sizing: content-box;
+        --darkblue: #243d83;
+        --lightblue: #6592cf;
+        --yellow: #eeb850;
+    }
+
+    .main {
+        height: 300px;
+        width: 400px;
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        position: absolute;
+        overflow: hidden;
+        background-color:var(--darkblue);
+
+        &>div {
+            height: 100px;
+            aspect-ratio: 1;
+            position: absolute;
+            border-radius: 50%;
+            /* opacity: 0.5; */
+            background-color: red;
+            box-shadow: 60px 0 var(--lightblue), -60px 0 var(--lightblue), 0 60px var(--lightblue), 0 -60px var(--lightblue);
+            transform: rotate(45deg);
+
+            &:before {
+                content: '';
+                position: absolute;
+                transform: translate(-10px, -10px);
+                height: 120px;
+                aspect-ratio: 1;
+                background-color: var(--darkblue);
+            }
+            &:after{
+                content: '';
+                position: absolute;
+                transform: translate(20px, 20px);
+                border-radius:50%;
+                height: 60px;
+                aspect-ratio: 1;
+                background-color: var(--yellow);
+
+            }
+
+        }
+    }
+</style>
---
+++ Battles/Battle_015/84-Junction.svelte
@@ -0,0 +1,53 @@
+<div class="main">
+	<div></div>
+	<div></div>
+	<div></div>
+	<div></div>
+</div>
+
+<style>
+    * {
+        margin: 0;
+        padding: 0;
+        box-sizing: content-box;
+        --black: #191919;
+        --dark: #a64942;
+        --light: #fe5f55;
+    }
+
+    .main {
+        height: 300px;
+        width: 400px;
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        position: absolute;
+        overflow: hidden;
+        background-color:var(--black);
+
+        &>div {
+            position:absolute;
+            height: 130px;
+            width: 40px;
+            background-color: var(--light);
+            transform:translateY(-85px);
+            border-bottom-left-radius:20px;
+            border-bottom-right-radius:20px;
+            /* opacity: 0.5; */
+        }
+        &>div:nth-child(2){
+            transform:rotate(180deg)translateY(-85px)
+        }
+        &>div:nth-child(3){
+            height:180px;
+            transform: translate(110px) rotate(90deg);
+            background-color: var(--dark);
+        }
+        &>div:nth-child(4){
+            height:180px;
+            transform: translate(-110px) rotate(-90deg);
+            background-color: var(--dark);
+        }
+    
+    }
+</style>
