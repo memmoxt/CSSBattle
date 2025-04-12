70e754c419ddeffa01ef109d7f0e92e0357ebde2
memmoxt <willianfujii@gmail.com>
2025-04-11 21:45:47 -0300

    april-day-12

---
+++ Battles/Battle_015/87-Building-Blocks.svelte
@@ -0,0 +1,68 @@
+<div class="main">
+	<div></div>
+	<div></div>
+	<div></div>
+</div>
+
+<style>
+	* {
+	    margin: 0;
+	    padding: 0;
+	    box-sizing: content-box;
+	    --yellow: #f3ac3c;
+	    --green: #1a4341;
+	}
+
+	.main {
+	    height: 300px;
+	    width: 400px;
+	    display: flex;
+	    justify-content: center;
+	    align-items: center;
+	    position: absolute;
+	    overflow: hidden;
+        background-color: var(--yellow);
+
+	    &>div:nth-child(1),
+	    &>div:nth-child(2) {
+	        position: absolute;
+	        height: 101px;
+	        aspect-ratio: 1;
+	        background-color: var(--green);
+	        border-radius: 10px;
+	        transform: translate(-85px, -35px);
+	        /* opacity: 0.5; */
+
+	        &:before {
+	            position: absolute;
+	            content: '';
+	            height: 51px;
+	            width: 100px;
+	            border-radius: 10px;
+	            transform: translate(51px, 0px);
+	            background-color: var(--green);
+	        }
+
+	        &:after {
+	            position: absolute;
+	            content: '';
+	            height: 20px;
+	            width: 20px;
+	            background-color: var(--green);
+	            transform: translate(95px, 44px);
+	        }
+	    }
+
+	    &>div:nth-child(2) {
+	        transform: scale(-1) translate(-86px, -36px);
+	    }
+
+        &>div:nth-child(3){
+            position:absolute;
+            height:70px;
+            aspect-ratio:1;
+            background-color:var(--yellow);
+            border-radius:10px;
+        }
+	}
+</style>
--- src/routes/CSS_Battle.svelte
+++ src/routes/CSS_Battle.svelte
@@ -1,12 +1,56 @@
 <div class="main">
-	<div>
-        <div></div>
-        <div></div>
-        <div></div>
-        <div></div>
-    </div>
+	<div></div>
 </div>
 
 <style>
-	@import './cssbattle.css';
+    * {
+    margin: 0;
+    padding: 0;
+    box-sizing: content-box;
+    --yellow: #e3cc9e;
+    --red: #995757;
+    --white: #ffffff;
+    --dark: #6c3232;
+}
+
+.main {
+    height: 300px;
+    width: 400px;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    position: absolute;
+    overflow: hidden;
+        background-color:var(--yellow);
+
+    &>div {
+        position: absolute;
+        height: 180px;
+        width: 300px;
+        background-color: var(--red);
+
+        &:before {
+            position: absolute;
+            content: '';
+            height: 40px;
+            width: 300px;
+            background-color: var(--red);
+            bottom: -20px;
+            border-radius: 50%;
+            box-shadow: 0 -180px var(--white);
+        }
+
+        &:after {
+            position: absolute;
+            content: '';
+            height: 20px;
+            width: 140px;
+            background-color: var(--dark);
+            border-radius: 50%;
+            transform: translate(80px, -10px);
+        }
+    }
+
+}
+
 </style>
