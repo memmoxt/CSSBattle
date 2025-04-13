15384a5a12f3bdb7e263ba6c3befb8ca0c7979cc
memmoxt <willianfujii@gmail.com>
2025-04-12 21:35:04 -0300

    april-day-13
    daily & 88 cssbattle

---
+++ Battles/Battle_015/88-Tight-Corner.svelte
@@ -0,0 +1,61 @@
+<div class="main">
+	<div></div>
+	<div></div>
+</div>
+
+<style>
+* {
+    margin: 0;
+    padding: 0;
+    box-sizing: content-box;
+    --white: #f7f3da;
+    --red: #d25b70;
+}
+
+.main {
+    height: 300px;
+    width: 400px;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    position: absolute;
+    overflow: hidden;
+    background-color: var(--white);
+
+    &>div:nth-child(1),
+    &>div:nth-child(2) {
+        position: absolute;
+        height: 20px;
+        width: 20px;
+        background-color: var(--red);
+        transform: translate(10px, -10px);
+
+        &:before {
+            content: '';
+            position: absolute;
+            background-color: var(--red);
+            height: 40px;
+            width: 200px;
+            transform: translate(0px, 20px);
+            border-bottom-left-radius: 10px;
+        }
+
+        &:after {
+            content: '';
+            position: absolute;
+            height: 150px;
+            width: 200px;
+            border-bottom-left-radius: 20px;
+            left: 0px;
+            bottom: 0px;
+            background-color: var(--white);
+            transform: translate(0px, 0px);
+        }
+    }
+
+    &>div:nth-child(2) {
+        transform: scale(-1) translate(10px, -10px);
+    }
+
+}
+</style>
