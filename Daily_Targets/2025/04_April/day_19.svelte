40b8ea78949a3c858006699fb502300a1c3c5ddc
memmoxt <willianfujii@gmail.com>
2025-04-18 21:28:10 -0300

    april-day-19
    daily & 94 Ripples cssbattle

---
+++ Daily_Targets/2025/04_April/day_19.svelte
@@ -0,0 +1,40 @@
+
+<div class="main">
+	<div></div>
+</div>
+
+<style>
+	* {
+		margin: 0;
+		padding: 0;
+		box-sizing: content-box;
+		--white: #fefaed;
+		--beige: #f9d9bf;
+		--brown: #d4a29f;
+	}
+
+	.main {
+		height: 300px;
+		width: 400px;
+		display: flex;
+		justify-content: center;
+		align-items: center;
+		position: absolute;
+        background-color:var(--white);
+        &>div{
+            height:180px;
+            width:240px;
+            background-color:var(--beige);
+            /* opacity:0.5; */
+            &:before{
+                content:'';
+                position:absolute;
+                height:90px;
+                width:60px;
+                background-color:var(--brown);
+                box-shadow:180px 90px var(--brown);
+            }
+
+        }
+	}
+</style>
