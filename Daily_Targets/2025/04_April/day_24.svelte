74598b0a549143fa9cfe9d6f8dd8846220a7ffc6
memmoxt <willianfujii@gmail.com>
2025-04-23 23:37:26 -0300

    april-day-24
    daily & 99 cssbattle

---
+++ Daily_Targets/2025/04_April/day_24.svelte
@@ -0,0 +1,39 @@
+<div class="main">
+	<div></div>
+</div>
+
+<style>
+	* {
+		margin: 0;
+		padding: 0;
+		box-sizing: content-box;
+        --green:#5DCBA3;
+        --purple:#6255AA;
+	}
+
+	.main {
+		height: 300px;
+		width: 400px;
+		display: flex;
+		justify-content: center;
+		align-items: center;
+		position: absolute;
+        background-color:var(--purple);
+        &>div{
+            position:absolute;
+            height:50px;
+            width:350px;
+            background-color:var(--green);
+            &:before{
+                content:'';
+                position:absolute;
+                height:50px;
+                width:50px;
+                background-color:var(--green);
+                transform:translate(250px,50px);
+                box-shadow: 50px 50px var(--green), 0px -100px var(--green), 50px -150px var(--green);
+            }
+        }
+	}
+</style>
+
