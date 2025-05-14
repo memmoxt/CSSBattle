a5e14227a98b12eebfaa433fa6f7f4aa5715e246
memmoxt <willianfujii@gmail.com>
2025-05-14 00:05:39 -0300

    may-day-14
    daily cssbattle

--- src/routes/CSS_Battle.svelte
+++ src/routes/CSS_Battle.svelte
@@ -7,9 +7,9 @@
 		margin: 0;
 		padding: 0;
 		box-sizing: content-box;
-        --yellow:#E3CC9E;
-        --red:#D95F5B;
-        background-color:var(--yellow);
+		--blue: #243d83;
+		--lightblue: #5aa4b7;
+        background-color:var(--blue);
 	}
 	.main {
 		height: 300px;
@@ -21,31 +21,29 @@
 		overflow: hidden;
         &>div{
             position:absolute;
-            height:80px;
-            aspect-ratio:1;
-            background-color:var(--red);
-            border-radius:50%;
-            transform:translate(-80px);
-            box-shadow:160px 0 var(--red);
+            border-top: 0px solid transparent;
+            border-right: 80px solid transparent;
+            border-bottom: 80px solid var(--lightblue);
+            border-left: 80px solid transparent;
+            transform:translate(0px, -80px);
             &:before{
                 content:'';
                 position:absolute;
-                width:240px;
-                aspect-ratio:1;
-                border-radius:50%;
-                transform:translate(0px,-80px);
-                clip-path:polygon(0px 0px , 240px 0px, 240px 120px, 0px 120px);
-                background-color:var(--red);
+                height:80px;
+                width:160px;
+                background-color:var(--lightblue);
+                transform:translate(-80px,80px);
             }
             &:after{
                 content:'';
                 position:absolute;
-                height:200px;
-                width:40px;
-                transform:translate(100px);
-                background-color:var(--red);
+                height:80px;
+                aspect-ratio:1;
+                background-color:var(--blue);
+                border-radius:50%;
+                transform:translate(-120px,80px);
+                box-shadow: 160px 0 var(--blue), 80px 40px 0 40px var(--lightblue);
             }
         }
-
 	}
 </style>
