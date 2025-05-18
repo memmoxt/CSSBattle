c121575c2864562ae10af011dabe32bb220fbce3
MemmoXT <willianfujii@gmail.com>
2025-05-17 22:30:46 -0300

    may-day-18
    daily-target

--- src/routes/CSS_Battle.svelte
+++ src/routes/CSS_Battle.svelte
@@ -1,5 +1,6 @@
 <div class="main">
 	<div></div>
+	<div></div>
 </div>
 
 <style>
@@ -7,10 +8,10 @@
 		margin: 0;
 		padding: 0;
 		box-sizing: content-box;
-		--green: #51a499;
-		--yellow: #eac049;
-        --black:#282828;
-        background-color:var(--green);
+		--beige: #f3ead2;
+		--black: #4c455b;
+		--slate: #c0d6e7;
+		--red: #d96c7b;
 	}
 	.main {
 		height: 300px;
@@ -20,27 +21,29 @@
 		align-items: center;
 		position: absolute;
 		overflow: hidden;
-		& > div {
+        background-color:var(--beige);
+		& > div:nth-child(1) {
 			position: absolute;
-			height: 120px;
-			aspect-ratio: 1;
-			background-color: var(--yellow);
-			box-shadow:
-				-40px 0px var(--yellow),
-				40px 0px var(--yellow),
-				0px -40px var(--yellow),
-                0px 40px var(--yellow);
-            &:before,&:after{
-                content:'';
-                position:absolute;
-                height:200px;
-                width:40px;
-                transform:translate(40px, -40px);
-                background-color:var(--black);
-            }
-            &:after{
-                transform:translate(40px, -40px) rotate(90deg);
-            }
+			height: 50px;
+			width: 150px;
+			background-color: var(--red);
+			transform: rotate(-45deg) translate(0px, 50px);
+			&:after {
+				content: '';
+				position: absolute;
+				height: 150px;
+				width: 50px;
+                transform:translate(-0px,-100px);
+				background-color: var(--black);
+			}
+		}
+		& > div:nth-child(2) {
+			position: absolute;
+			border-top: 70px solid var(--slate);
+			border-right: 70px solid transparent;
+			border-bottom: 70px solid transparent;
+			border-left: 70px solid transparent;
+			transform: translate(0px, 35px);
 		}
 	}
 </style>
