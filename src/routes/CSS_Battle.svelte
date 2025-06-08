4cd1f5083c377d9fe9bf855bc5103efef48a64be
MemmoXT <willianfujii@gmail.com>
2025-06-08 10:54:07 -0300

    june-day-8

--- src/routes/CSS_Battle.svelte
+++ src/routes/CSS_Battle.svelte
@@ -1,8 +1,6 @@
 <div class="main">
-	<div class="outer"></div>
-	<div class="outer"></div>
-	<div class="center"></div>
-	<div class="center"></div>
+	<div></div>
+	<div></div>
 </div>
 
 <style>
@@ -10,9 +8,8 @@
 		margin: 0;
 		padding: 0;
 		box-sizing: content-box;
-		--blue: #5aa4b7;
-		--black: #000000;
-        background-color:var(--blue);
+		--yellow: #f2eaa8;
+		--red: #ee7667;
 	}
 	.main {
 		height: 300px;
@@ -22,43 +19,49 @@
 		align-items: center;
 		position: absolute;
 		overflow: hidden;
-	}
-	.center:nth-child(3),
-	.center:nth-child(3):before,
-	.center:nth-child(4),
-	.center:nth-child(4):before {
-		position: absolute;
-		height: 80px;
-		width: 20px;
-		background-color: var(--black);
-		border-radius: 20px/20px;
-	}
-	.center:nth-child(3):before,
-	.center:nth-child(4):before {
-		content: '';
-		transform: rotate(-90deg);
-	}
+        background-color:var(--yellow);
+		& > div:nth-child(1) {
+			height: 200px;
+			aspect-ratio: 1;
+			background-color: var(--red);
+			border-radius: 50%;
+			overflow: hidden;
+			position: absolute;
+			&:before,
+			&:after {
+				content: '';
+				position: absolute;
+				height: 200px;
+				width: 60px;
+				transform: translate(70px);
+				background-color: var(--yellow);
+			}
+			&:after {
+				width: 40px;
+				transform: translate(80px) rotate(-90deg);
+			}
+		}
 
-	.center:nth-child(4) {
-		transform: rotate(-45deg);
-	}
-
-	.outer:nth-child(1) {
-		height: 20px;
-		width: 200px;
-		position: absolute;
-		border-radius: 50px/50px;
-		background-color: var(--black);
-		transform: translateY(-70px);
-		box-shadow: 0 140px var(--black);
-	}
-	.outer:nth-child(2) {
-		height: 20px;
-		width: 200px;
-		position: absolute;
-		border-radius: 50px/50px;
-		background-color: var(--black);
-		transform: translateX(-70px) rotate(-90deg);
-		box-shadow: 0 140px var(--black);
+		& > div:nth-child(2) {
+			height: 200px;
+			aspect-ratio: 1;
+			position: absolute;
+            overflow: hidden;
+			&:before,
+			&:after {
+				content: '';
+				position: absolute;
+				height: 100px;
+				width: 20px;
+				background-color: var(--red);
+				box-shadow: 0 140px var(--red);
+				transform: translate(90px, -20px);
+			}
+			&:after {
+				transform: translate(160px, 60px) rotate(90deg);
+				height: 80px;
+				width: 20px;
+			}
+		}
 	}
 </style>
