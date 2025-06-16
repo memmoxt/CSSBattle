2b07602efbd4633bb861e6aad5dae16fd4d15603
MemmoXT <willianfujii@gmail.com>
2025-06-15 22:24:10 -0300

    june-day-16

--- src/routes/CSS_Battle.svelte
+++ src/routes/CSS_Battle.svelte
@@ -1,19 +1,11 @@
 <div class="main">
-	<div></div>
-	<div></div>
-	<div></div>
-	<div></div>
-	<div></div>
-	<div></div>
-	<div></div>
-	<div></div>
-	<div></div>
-	<div></div>
-	<div></div>
-	<div></div>
-	<div></div>
-	<div></div>
-	<div></div>
+	<div>
+		<div></div>
+		<div></div>
+	</div>
+	<div>
+		<div></div>
+	</div>
 </div>
 
 <style>
@@ -21,30 +13,54 @@
 		margin: 0;
 		padding: 0;
 		box-sizing: content-box;
-		--green: #97ae41;
-		--blue: #334f9f;
+		--red: #d95362;
+		--white: #d9d9d9;
+        background-color: var(--red);
 	}
 	.main {
 		height: 300px;
 		width: 400px;
-		display: grid;
-
+		display: flex;
 		justify-content: center;
 		align-items: center;
 		position: absolute;
 		overflow: hidden;
-		grid-template-columns: 40px 1fr 40px;
-		grid-template-rows: 90px repeat(3, 1fr) 90px;
-		& > div {
-			position: relative;
-			height: 100%;
-			width: 100%;
+		& > div:nth-child(1) {
+			position: absolute;
+			top: 28px;
+			height: 164px;
+			width: 328px;
+			/* background-color: black; */
+			& > div {
+				position: absolute;
+				height: 164px;
+				aspect-ratio: 1;
+				background-color: var(--white);
+				box-shadow: 164px 0 var(--red);
+				&:before {
+					content: '';
+					position: absolute;
+					height: 90px;
+					aspect-ratio: 1;
+					border-radius: 50%;
+					translate: 36px 36px;
+					background-color: var(--red);
+					box-shadow: 165px 0 var(--white);
+				}
+			}
 		}
-		& > div:nth-child(odd) {
-			background-color: var(--blue);
-		}
-		& > div:nth-child(even) {
-			background-color: var(--green);
+		& > div:nth-child(2) {
+			position: absolute;
+			height: 90px;
+			width: 328px;
+			bottom: 18px;
+
+			& > div {
+				border-top: 80px solid var(--white);
+				border-right: 164px solid transparent;
+				border-left: 164px solid transparent;
+				border-bottom: 0px solid transparent;
+			}
 		}
 	}
 </style>
