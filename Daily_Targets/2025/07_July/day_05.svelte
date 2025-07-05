fe170ca19d812ff20f274038bee96ce9b6ecad01
MemmoXT <willianfujii@gmail.com>
2025-07-04 21:21:36 -0300

    july-day-5

--- Daily_Targets/2025/07_July/day_05.svelte
+++ Daily_Targets/2025/07_July/day_05.svelte
@@ -18,21 +18,21 @@
 		justify-content: center;
 		align-items: center;
 		overflow: hidden;
-        background-color:var(--yellow);
+		background-color: var(--yellow);
 		& > div {
 			height: 60px;
 			width: 340px;
 			background-color: var(--white);
-            border-radius:10px/10px;
-            box-shadow: 0 0 0 10px var(--black);
-            &::before{
-                content:'';
-                position:absolute;
-                height:44px;
-                width:10px;
-                background-color:var(--black);
-                translate:14px 8px;
-            }
+			border-radius: 10px/10px;
+			box-shadow: 0 0 0 10px var(--black);
+			&::before {
+				content: '';
+				position: absolute;
+				height: 44px;
+				width: 10px;
+				background-color: var(--black);
+				translate: 14px 8px;
+			}
 		}
 	}
 </style>
