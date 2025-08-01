ee5d6ffba9bd77e799626e4200a5658422d768d1
MemmoXT <willianfujii@gmail.com>
2025-07-31 22:02:35 -0300

    august-day-01

---
+++ Daily_Targets/2025/07_July/day_30.svelte
@@ -0,0 +1,52 @@
+<div class="main">
+	<div></div>
+</div>
+
+<style>
+	* {
+		margin: 0;
+		padding: 0;
+		box-sizing: content-box;
+		--green: #48bf7d;
+		--yellow: #feff58;
+		--blue: #3e5aa9;
+        background-color:var(--green);
+	}
+	.main {
+		height: 300px;
+		width: 400px;
+		display: flex;
+		justify-content: center;
+		align-items: center;
+		overflow: hidden;
+		position: absolute;
+		& > div {
+			height: 140px;
+			aspect-ratio: 1;
+			background-color: var(--green);
+			border-radius: 50%;
+			translate: -15px;
+			box-shadow: 0 0 0 30px var(--yellow);
+
+			&:before {
+				content: '';
+				position: absolute;
+				height: 200px;
+				width: 30px;
+				background-color: var(--blue);
+				box-shadow:
+					30px 0px var(--yellow),
+					60px 0px var(--yellow);
+				translate: 70px -30px;
+			}
+			&:after {
+				content: '';
+				position: absolute;
+				height: 200px;
+				width: 70px;
+				background-color: var(--green);
+                translate:130px -60px;
+			}
+		}
+	}
+</style>
---
+++ Daily_Targets/2025/07_July/day_31.svelte
@@ -0,0 +1,48 @@
+<div class="main">
+	<div></div>
+</div>
+
+<style>
+	* {
+		margin: 0;
+		padding: 0;
+		box-sizing: content-box;
+		--green: #23384b;
+		--yellow: #f0cd48;
+		background-color: var(--green);
+	}
+	.main {
+		height: 300px;
+		width: 400px;
+		display: flex;
+		justify-content: center;
+		align-items: center;
+		overflow: hidden;
+		position: absolute;
+		& > div {
+			height: 155px;
+			width: 50px;
+			background-color: var(--yellow);
+			translate: -65px -53px;
+			border-top-left-radius: 25px;
+			border-top-right-radius: 25px;
+			&:before {
+				content: '';
+				position: absolute;
+				height: 50px;
+				width: 180px;
+				background-color: var(--yellow);
+				translate: 0px 105px;
+			}
+			&:after {
+				content: '';
+				position: absolute;
+				height: 260px;
+				width: 50px;
+				background-color: var(--yellow);
+				translate: 130px 0px;
+				border-radius: 25px/25px;
+			}
+		}
+	}
+</style>
