dc37e15cb2a72a91219d2a77066097c9ac76c13c
MemmoXT <willianfujii@gmail.com>
2025-08-12 06:32:42 -0300

    august-day-11

---
+++ Daily_Targets/2025/08_August/day_11.svelte
@@ -0,0 +1,56 @@
+<div class="main">
+	<div></div>
+</div>
+
+<style>
+	* {
+		margin: 0;
+		padding: 0;
+		/* box-sizing: content-box; */
+		box-sizing: border-box;
+		--pink: #b673a6;
+		--blue: #619ebf;
+		--white: #d9d9d9;
+	}
+	.main {
+		height: 300px;
+		width: 400px;
+		display: flex;
+		justify-content: center;
+		align-items: center;
+		overflow: hidden;
+		position: absolute;
+		& > div {
+			position: absolute;
+			height: 100%;
+			width: 100%;
+			background-color: var(--pink);
+			&:before {
+				content: '';
+				position: absolute;
+				height: 150px;
+				aspect-ratio: 1;
+				background-color: var(--blue);
+				translate: -30px 180px;
+				border-top-right-radius: 10px;
+				border-top-left-radius: 10px;
+				box-shadow:
+					310px 0 var(--blue),
+					0 0 0 30px var(--white),
+					310px 0 0 30px var(--white);
+			}
+			&:after {
+				content: '';
+				position: absolute;
+				height: 40px;
+				aspect-ratio: 1;
+				background-color: var(--white);
+				translate: 180px 130px;
+				border-radius: 50%;
+				box-shadow:
+					80px 30px var(--blue),
+					-80px 30px var(--blue);
+			}
+		}
+	}
+</style>
