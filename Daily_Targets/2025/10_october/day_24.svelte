22e8a71f090f7fc174ab4b89115e9de1af89c517
MemmoXT <willianfujii@gmail.com>
2025-10-25 09:58:13 -0300

    october-day-25

---
+++ Daily_Targets/2025/10_october/day_24.svelte
@@ -0,0 +1,43 @@
+<div class="main">
+	<div></div>
+</div>
+
+<style>
+	* {
+		margin: 0;
+		padding: 0;
+		box-sizing: border-box;
+		--blue: #4f77ff;
+		--white: #ffeff2;
+        background-color: var(--blue);
+	}
+	.main {
+		height: 300px;
+		width: 400px;
+		display: flex;
+		justify-content: center;
+		align-items: center;
+		overflow: hidden;
+		position: absolute;
+		& > div {
+			position: absolute;
+			height: 50px;
+			width: 50px;
+			background-color: var(--white);
+			box-shadow:
+				-125px -75px var(--blue),
+				-145px -95px 0px 20px var(--white),
+				125px 75px var(--blue),
+				145px 95px 0 20px var(--white);
+                &:before{
+                content:'';
+                position:absolute;
+                height: 40px;
+                width: 40px;
+                translate: -165px -115px;
+                box-shadow: 340px 240px var(--blue);
+                background-color: var(--blue);
+            }
+		}
+	}
+</style>
