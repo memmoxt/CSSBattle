11befe99f4b08255d10d750dbc3581df5fec1be7
MemmoXT <willianfujii@gmail.com>
2025-11-29 14:24:00 -0300

    november-day-29

---
+++ Daily_Targets/2025/11_november/day_28.svelte
@@ -0,0 +1,48 @@
+<div class="main">
+	<div></div>
+</div>
+
+<style>
+	* {
+		margin: 0;
+		padding: 0;
+		box-sizing: border-box;
+        --orange:#D86F45;
+        --white:#F5D6B4;
+        --brown:#734939;
+        background-color: var(--orange);
+
+	}
+	.main {
+		height: 300px;
+		width: 400px;
+		display: flex;
+		justify-content: center;
+		align-items: center;
+		overflow: hidden;
+		position: absolute;
+        &>div{
+            height: 160px;
+            width:200px;
+            background-color: var(--brown);
+            translate: -20px -20px;
+            &:before{
+                content:'';
+                position:absolute;
+                height: 160px;
+                width: 200px;
+                background-color:var(--white);
+                border-radius: 20px / 20px;
+                translate: 20px 20px;
+            }
+            &:after{
+                content:'';
+                position:absolute;
+                height: 40px;
+                width:100px;
+                background-color:var(--orange);
+                translate: 140px 60px;
+            }
+        }
+	}
+</style>
