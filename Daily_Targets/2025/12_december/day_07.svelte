9b1e4cf148fda3f7fcfe3da437bfeaf6c45b2586
MemmoXT <willianfujii@gmail.com>
2025-12-07 09:42:16 -0300

    december-day-07

---
+++ Daily_Targets/2025/12_december/day_07.svelte
@@ -0,0 +1,46 @@
+<div class="main">
+	<div><div></div></div>
+</div>
+
+<style>
+	* {
+		margin: 0;
+		padding: 0;
+		box-sizing: border-box;
+		--red: #e3516e;
+		--brown: #511818;
+        background-color: var(--red);
+	}
+	.main {
+		height: 300px;
+		width: 400px;
+		display: flex;
+		justify-content: center;
+		align-items: center;
+		overflow: hidden;
+		position: absolute;
+		& > div {
+			height: 100px;
+			width: 220px;
+			background-color: var(--brown);
+            &:before{
+                content:'';
+                position:absolute;
+                height:80px;
+                aspect-ratio:1;
+                background-color: var(--red);
+                translate: 70px -40px;
+            }
+            &:after{
+                content:'';
+                position:absolute;
+                height: 40px;
+                aspect-ratio:1;
+                background-color: var(--brown);
+                border-radius:50px;
+                translate: 90px -20px;
+                box-shadow: 0 140px var(--brown);
+            }
+		}
+	}
+</style>
--- src/routes/CSS_Battle.svelte
+++ src/routes/CSS_Battle.svelte
@@ -7,9 +7,9 @@
 		margin: 0;
 		padding: 0;
 		box-sizing: border-box;
-		--blue: #243d83;
-		--yellow: #f0cd48;
-        background-color:var(--blue);
+		--red: #e3516e;
+		--brown: #511818;
+        background-color: var(--red);
 	}
 	.main {
 		height: 300px;
@@ -20,20 +20,27 @@
 		overflow: hidden;
 		position: absolute;
 		& > div {
-			height: 150px;
-			aspect-ratio: 1;
-			background-color: var(--yellow);
-			border-radius: 40px / 40px;
-			border: 10px solid var(--yellow);
-			overflow: hidden;
-			& > div {
-				height: 300px;
-				border-radius: 50%;
-				aspect-ratio: 1;
-				background-color: transparent;
-				translate: -85px 75px;
-				box-shadow: 0 0 0 50px var(--blue);
-			}
+			height: 100px;
+			width: 220px;
+			background-color: var(--brown);
+            &:before{
+                content:'';
+                position:absolute;
+                height:80px;
+                aspect-ratio:1;
+                background-color: var(--red);
+                translate: 70px -40px;
+            }
+            &:after{
+                content:'';
+                position:absolute;
+                height: 40px;
+                aspect-ratio:1;
+                background-color: var(--brown);
+                border-radius:50px;
+                translate: 90px -20px;
+                box-shadow: 0 140px var(--brown);
+            }
 		}
 	}
 </style>
