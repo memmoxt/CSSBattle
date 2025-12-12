aa992ec67986f177f6e5bce4b797ec2ebc697d6a
MemmoXT <willianfujii@gmail.com>
2025-12-12 02:13:57 -0300

    december-day-12

---
+++ Daily_Targets/2025/12_december/day_11.svelte
@@ -0,0 +1,41 @@
+<div class="main">
+	<div></div>
+</div>
+
+<style>
+	* {
+		margin: 0;
+		padding: 0;
+		box-sizing: border-box;
+		--white: #f7e5fb;
+		--purple: #62306d;
+        background-color: var(--white);
+	}
+	.main {
+		height: 300px;
+		width: 400px;
+		display: flex;
+		justify-content: center;
+		align-items: center;
+		overflow: hidden;
+		position: absolute;
+		& > div {
+			height: 100px;
+			aspect-ratio: 1;
+			background-color: var(--white);
+			border-radius: 50%;
+			box-shadow: 0 0 0 30px var(--purple);
+			&:before {
+				content: '';
+				position: absolute;
+				height: 30px;
+				width: 60px;
+				background-color: var(--purple);
+				transform: translate(87px, 97px) rotate(45deg);
+				border-radius: 50px;
+			}
+		}
+	}
+</style>
+
+
---
+++ Daily_Targets/2025/12_december/day_12.svelte
@@ -0,0 +1,61 @@
+<div class="main">
+	<div></div>
+	<div></div>
+</div>
+
+<style>
+	* {
+		margin: 0;
+		padding: 0;
+		box-sizing: border-box;
+		--green: #63b476;
+		--blue: #243d83;
+		--dark: #000e37;
+		--white: #ffffff;
+        background-color:var(--green);
+	}
+	.main {
+		height: 300px;
+		width: 400px;
+		display: flex;
+		justify-content: center;
+		align-items: center;
+		overflow: hidden;
+		position: absolute;
+		& > div:nth-child(1) {
+			content: '';
+			position: absolute;
+			height: 30px;
+			width: 50px;
+			background-color: var(--white);
+			translate: 95px 0px;
+			border-radius: 50px;
+			&:before {
+				content: '';
+				position: absolute;
+				height: 100px;
+				width: 180px;
+				translate: -160px -35px;
+				border-radius: 50px;
+				background-color: var(--blue);
+			}
+			&:after {
+				content: '';
+				position: absolute;
+				height: 100px;
+				width: 100px;
+				background-color: var(--dark);
+				translate: -160px -35px;
+				border-radius: 50%;
+			}
+		}
+		& > div:nth-child(2) {
+			height: 30px;
+			width: 95px;
+			background-color: var(--white);
+			translate: -73px;
+			border-radius: 50px;
+		}
+	}
+</style>
+
