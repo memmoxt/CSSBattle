551777d9c596de2ee7eff5aa35283557314fc6ec
MemmoXT <willianfujii@gmail.com>
2025-12-27 11:55:46 -0300

    december-day-27

--- Daily_Targets/2025/12_december/day_27.svelte
+++ Daily_Targets/2025/12_december/day_27.svelte
@@ -6,7 +6,7 @@
 	* {
 		margin: 0;
 		padding: 0;
-		box-sizing: border-box;
+		box-sizing:content-box;
         --yellow:#F9DDC6;
         --purple:#594C94;
         background-color: var(--yellow);
@@ -23,11 +23,11 @@
             height: 120px;
             border-radius:50%;
             aspect-ratio:1;
-            background-color: var(--purple);
+            background-color: var(--purple); 
             &:before,&:after{
                 content:'';
                 position:absolute;
-                height: 100px;
+                height: 60px;
                 aspect-ratio:1;
                 background-color: var(--purple);
                 border: 20px solid var(--yellow);
@@ -40,3 +40,4 @@
         
 	}
 </style>
+
