5f45ca37733681223d4b634c1b95b24b9b5a3fd3
MemmoXT <willianfujii@gmail.com>
2026-01-10 10:48:58 -0300

    january-day-10

---
+++ Daily_Targets/2026/day_08.svelte
@@ -0,0 +1,47 @@
+<div class="main">
+	<div></div>
+</div>
+
+<style>
+	* {
+		margin: 0;
+		padding: 0;
+		box-sizing: border-box;
+		--yellow: #f2eaa8;
+		--red: #ee7667;
+		--green: #84be8d;
+        background-color: var(--yellow);
+	}
+	.main {
+		height: 300px;
+		width: 400px;
+		display: flex;
+		justify-content: center;
+		align-items: center;
+		overflow: hidden;
+		position: absolute;
+		& > div {
+			height: 40px;
+			width: 90px;
+			background-color: var(--red);
+			translate: -120px -90px;
+			box-shadow:
+				240px 0px var(--red),
+				0px 180px var(--green),
+				240px 180px var(--green);
+			&:before {
+				content: '';
+				position: absolute;
+				height: 80px;
+				width: 40px;
+				background-color: var(--red);
+				translate: 25px 10px;
+				box-shadow:
+					240px 0px var(--red),
+					0px 120px var(--green),
+					240px 120px var(--green);
+			}
+
+		}
+	}
+</style>
---
+++ Daily_Targets/2026/day_09.svelte
@@ -0,0 +1,40 @@
+<div class="main">
+	<div></div>
+</div>
+
+<style>
+	* {
+		margin: 0;
+		padding: 0;
+		box-sizing: border-box;
+		--marine: #2b2a4a;
+		--white: #ece3df;
+        background-color: var(--white);
+	}
+	.main {
+		height: 300px;
+		width: 400px;
+		display: flex;
+		justify-content: center;
+		align-items: center;
+		overflow: hidden;
+		position: absolute;
+		& > div {
+			position: absolute;
+			height: 150px;
+			width: 260px;
+			background-color: var(--marine);
+			translate: 0px -100px;
+            box-shadow: 0px 200px var(--marine);
+			&:before {
+				content: '';
+				position: absolute;
+				height: 25px;
+				width: 80px;
+				background-color: var(--marine);
+                translate: 260px 125px;
+                box-shadow: -340px 75px var(--marine);
+			}
+		}
+	}
+</style>
