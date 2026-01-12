15ec81ef797171f9985ec784b673feabcf120227
MemmoXT <willianfujii@gmail.com>
2026-01-12 09:35:26 -0300

    january-day-12

---
+++ Daily_Targets/2026/day_11.svelte
@@ -0,0 +1,44 @@
+<div class="main">
+	<div></div>
+</div>
+
+<style>
+	* {
+		margin: 0;
+		padding: 0;
+		box-sizing: border-box;
+		--blue: #5aa4b7;
+		--white: #f9eaf1;
+		--black: #4c455b;
+        background-color: var(--blue);
+	}
+	.main {
+		height: 300px;
+		width: 400px;
+		display: flex;
+		justify-content: center;
+		align-items: center;
+		overflow: hidden;
+		position: absolute;
+		& > div {
+			height: 130px;
+			width: 120px;
+			translate: -40px 40px;
+			background-color: var(--black);
+			box-shadow: 80px -80px var(--white);
+			&:before {
+				content: '';
+				position: absolute;
+				height: 10px;
+				width: 40px;
+				background-color: var(--blue);
+				translate: 80px;
+				box-shadow:
+					0px 10px var(--white),
+					0px 20px var(--blue),
+					0 30px var(--black),
+					0 40px var(--blue);
+			}
+		}
+	}
+</style>
